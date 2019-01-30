ee9621d2029c21890370ea8e7221863aa3ca21f6
akalya23 <akalyaammu26@gmail.com>
2019-01-30 14:41:50 +0530

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,13 @@
+class abc
+{
+    public static void main (String[]args)
+    {
+        double fact=1;
+        for(int i=1;i<=8785856;i++)
+        {
+            fact=fact*i;
+        }
+
+        System.out.println(fact);
+    }
+}
